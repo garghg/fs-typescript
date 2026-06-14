d20db28657e9293fcde864c8eed5594511854092
Haardik Garg <hghaardik@gmail.com>
2026-06-14 13:26:53 -0700

    finished ex-9.17

---
+++ course/src/components/Content.tsx
@@ -0,0 +1,26 @@
+interface ContentProp {
+  name: string;
+  exerciseCount: number;
+}
+
+interface ContentProps {
+  courseParts: ContentProp[];
+}
+
+const Content = ({ courseParts }: ContentProps) => {
+  return (
+    <div>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+    </div>
+  );
+};
+
+export default Content;
