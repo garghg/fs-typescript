fe4675387c6b125323e4a171ae6ac0c4590a6dd3
Haardik Garg <hghaardik@gmail.com>
2026-06-15 16:15:34 -0700

    finished ex-9.25

--- patientor/backend/types.ts
+++ patientor/backend/types.ts
@@ -23,12 +23,64 @@ export const NewPatientSchema = z.object({
 export type Gender = (typeof Gender)[keyof typeof Gender];
 export type NewPatient = z.infer<typeof NewPatientSchema>;
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
 }
+
+const HealthCheckRating = {
+  Healthy: 0,
+  LowRisk: 1,
+  HighRisk: 2,
+  CriticalRisk: 3,
+} as const;
+
+type HealthCheckRating =
+  (typeof HealthCheckRating)[keyof typeof HealthCheckRating];
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
+
+export type EntryWithoutId = UnionOmit<Entry, "id">;
 
 export interface Patient extends NewPatient {
   id: string;
   entries: Entry[];
 }
-export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>;
+
+export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
